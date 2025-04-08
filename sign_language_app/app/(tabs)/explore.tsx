2005ee9334ce32745ca5e48aafbcdfadc700645c
BankUwU <bankwasd123@gmail.com>
2025-04-08 15:07:14 +0700

    Home page Update 2

--- sign_language_app/app/(tabs)/explore.tsx
+++ sign_language_app/app/(tabs)/explore.tsx
@@ -2,7 +2,7 @@ import { Ionicons } from '@expo/vector-icons';
 import React from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-export default function TabTwoScreen() {
+export default function TranslateScreen() {
   return (
     <View style={styles.container}>
       {/* Camera Preview Section */}
@@ -21,9 +21,6 @@ export default function TabTwoScreen() {
           <Ionicons name="close" size={20} color="white" />
         </TouchableOpacity>
       </View>
-
-      {/* Bottom Tab Bar */}
-      
     </View>
   );
 }
@@ -36,10 +33,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
   cameraContainer: {
-    height: 250,
+    height: 400,
     backgroundColor: '#d3d3d3',
     borderRadius: 12,
     justifyContent: 'center',
+    minHeight :300,
     alignItems: 'center',
   },
   cameraText: {
@@ -52,6 +50,7 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     padding: 16,
     marginVertical: 20,
+    minHeight: 100,
     position: 'relative',
   },
   translationText: {
@@ -67,13 +66,5 @@ const styles = StyleSheet.create({
     position: 'absolute',
     right: 10,
     top: 10,
-  },
-  tabBar: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    paddingVertical: 10,
-    backgroundColor: 'white',
-    borderTopWidth: 1,
-    borderTopColor: '#ccc',
   },
 });
